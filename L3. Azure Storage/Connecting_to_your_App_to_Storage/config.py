4ce9b579187e8f8dbb52a6fdf23d1c90e6195cd8
Salomi Margret M <samret149@gmail.com>
2025-10-06 11:37:46 +0530

    Update config.py

--- L3. Azure Storage/Connecting_to_your_App_to_Storage/config.py
+++ L3. Azure Storage/Connecting_to_your_App_to_Storage/config.py
@@ -5,14 +5,14 @@ basedir = os.path.abspath(os.path.dirname(__file__))
 class Config(object):
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'secret-key'
 
-    SQL_SERVER = os.environ.get('SQL_SERVER') or '[SQL_SERVER_GOES_HERE]'
-    SQL_DATABASE = os.environ.get('SQL_DATABASE') or '[SQL_DATABASE_GOES_HERE]'
-    SQL_USER_NAME = os.environ.get('SQL_USER_NAME') or '[SQL_USER_NAME_GOES_HERE]'
-    SQL_PASSWORD = os.environ.get('SQL_PASSWORD') or '[SQL_PASSWORD_GOES_HERE]'
+    SQL_SERVER = os.environ.get('SQL_SERVER') or 'my-zoo-server'
+    SQL_DATABASE = os.environ.get('SQL_DATABASE') or 'zoodb'
+    SQL_USER_NAME = os.environ.get('SQL_USER_NAME') or 'udacity-user'
+    SQL_PASSWORD = os.environ.get('SQL_PASSWORD') or 'Uda@12345'
     SQLALCHEMY_DATABASE_URI = 'mssql+pyodbc://' + SQL_USER_NAME + '@' + SQL_SERVER + ':' + SQL_PASSWORD + '@' + SQL_SERVER + ':1433/' + SQL_DATABASE + '?driver=ODBC+Driver+17+for+SQL+Server'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
 
-    BLOB_ACCOUNT = os.environ.get('BLOB_ACCOUNT') or '[BLOB_ACCOUNT_GOES_HERE]'
-    BLOB_STORAGE_KEY = os.environ.get('BLOB_STORAGE_KEY') or '[BLOB_STORAGE_KEY_GOES_HERE]'
-    BLOB_CONTAINER = os.environ.get('BLOB_CONTAINER') or '[BLOB_CONTAINER_GOES_HERE]'
+    BLOB_ACCOUNT = os.environ.get('BLOB_ACCOUNT') or 'zoo1store'
+    BLOB_STORAGE_KEY = os.environ.get('BLOB_STORAGE_KEY') or 'xuXDIpQ2eJa3vefVhteOHv+VRE/f83Xhusgqgdecmjl1Uy3lqXFhjFdcaQOd7IbrqJZI+7YuC8mB+ASt5Qy/YA=='
+    BLOB_CONTAINER = os.environ.get('BLOB_CONTAINER') or '$logs'
 
